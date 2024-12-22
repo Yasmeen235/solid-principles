26d23dedb62b8378c9d5a694c735a036c8d09475
Yasmeen235 <yasminkhater2006@gmail.com>
2024-12-22 23:19:08 +0200

    Add files via upload

---
+++ DIP.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod 
+class FrontEnd: 
+    def _init_(self, data_source): 
+        self.data_source = data_source 
+        def display_data(self): 
+            data = self.data_source.get_data() 
+            print("Display data:", data) 
+class DataSource(ABC): 
+    @abstractmethod 
+    def get_data(self): 
+        pass 
+ 
+class Database(DataSource): 
+    def get_data(self): 
+        return "Data from the database" 
+ 
+class API(DataSource): 
+    def get_data(self): 
+        return "Data from the API"
